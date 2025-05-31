fc957952bdc72dccc9a34c8566f0dff76be3fa71
Andrius Ordojan <andrius.or@gmail.com>
2025-05-31 22:01:00 +0200

    created file for archivebox integration

---
+++ archivebox.go
@@ -0,0 +1,5 @@
+package main
+
+func ArchiveBox() string {
+	return "ArchiveBox is a self-hosted web archive solution that allows you to save and organize web pages, links, and other online content for offline access and long-term storage."
+}
