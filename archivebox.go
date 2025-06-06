f53fad2f8fcf714d790ac9f75cdb43456320b2d7
Andrius Ordojan <andrius.or@gmail.com>
2025-06-06 23:17:06 +0200

    cleanup

--- archivebox.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func ArchiveBox() string {
-	return "ArchiveBox is a self-hosted web archive solution that allows you to save and organize web pages, links, and other online content for offline access and long-term storage."
-}
--- testdata/links_only.md
+++
@@ -1,7 +0,0 @@
----
-noto:
----
-
-https://betterstack.com/community/guides/logging/how-to-start-logging-with-python/#logging-errors-in-python
-
-https://last9.io/blog/python-logging-exceptions/
